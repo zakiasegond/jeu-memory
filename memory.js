ce0fdde2ddd2bf9f11e4f8d244eec125f4152855
zakiasegond <segondzakia@live.fr>
2018-06-29 09:13:50 +0200

    c 'est parti

--- memory.js
+++ memory.js
@@ -1,12 +1,12 @@
 /*var motifCarte=[1,1,2,2,3,3,4,4,5,5,6,6]; //Le jeu comporte 6 motifs différents qui sont numérotés de 1 à 6. //Le tableau est initialisé avec les numéros de motifs qui se suivent.
 var etatCarte = [0,0,0,0,0,0,0,0,0,0,0,0]; 
 var carteRetourner = [];*/
-var tapis = $('#face1');
 
 
-function cache(){
+
+
 
 	$('#face1').hide();
 
-}
+
 
